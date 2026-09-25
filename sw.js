b690ee7b82ae6588180e2ae815efa9a50cdea164
nathanfrenceasis-oss <nathanfrenceasis@gmail.com>
2026-09-25 11:17:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,34 @@
 const CACHE_NAME = "lumi-v3"; 
 const FILES_TO_CACHE = [
   "./",
-  "./index.html", 
-  "./daily.html", 
-  "./games.html", 
-  "./messages.html", 
-  "./style.css", 
+  "./index.html",
+  "./home.js",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
-]; 
+];
 
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-        .map(name => caches.delete(name))
-      );
-    })
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
